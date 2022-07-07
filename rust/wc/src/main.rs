e337554510973130de4d1a92b6785a516d1f09ce
310 <arthurcdx@yahoo.co.jp>
2022-07-07 11:57:08 +0900

    up

---
+++ rust/wc/src/main.rs
@@ -0,0 +1,36 @@
+#[derive(PartialEq)]
+enum CharKind {
+    Space,
+    Word,
+}
+
+fn wc(text :&str) -> u64 {
+
+    let mut count: u64 = 0;
+    let mut current_pos = CharKind::Space;
+
+    for c in text.chars() {
+        match c {
+            ' ' | '\n' => {
+                current_pos = CharKind::Space;
+            },
+            _ => {
+                if current_pos == CharKind::Space {
+                    current_pos = CharKind::Word;
+                    count += 1;
+                }
+            }
+        }
+    }
+    count
+}
+
+fn main() {
+    assert_eq!(wc("aa bb cc"), 3);
+    assert_eq!(wc("  aa bb cc"), 3);
+    assert_eq!(wc("aa bb cc  "), 3);
+    assert_eq!(wc("  aa  bb   cc  "), 3);
+    assert_eq!(wc("  aa"), 1);
+    assert_eq!(wc("  "), 0);
+    assert_eq!(wc(""), 0);
+}
