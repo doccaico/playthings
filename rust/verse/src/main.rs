d83d1aa592c0e076e3d28dfc518cb202375c9f3a
doccaico <hash_ext_luna@yahoo.co.jp>
2025-02-01 11:47:55 +0900

    up

--- rust/verse/src/main.rs
+++ rust/verse/src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::io;
 use std::process;
 
 use scraper::{Html, Selector};
@@ -12,7 +11,7 @@ fn help(code: i32) {
 A program to read bible.
 
     Usage: verse [Chapter] [Page]
-    Exsample: verse GEN 50
+    Exsample: verse GEN 1
         "#
     );
     process::exit(code);
@@ -27,25 +26,10 @@ fn main() {
     if args.len() != 3 {
         help(1);
     }
-    let root_url = &args[1];
-    let max: i32 = args[2]
+    let root_url: &String = &args[1];
+    let page: i32 = args[2]
         .parse()
         .unwrap_or_else(|_| panic!(r#""{}" not an integer"#, args[2]));
-
-    // 標準入力からページ(数値)を読み取る
-    let mut input_line = String::new();
-    let mut page: i32;
-    loop {
-        println!("読みたいページを入力してください。 (1 .. {max})");
-        io::stdin()
-            .read_line(&mut input_line)
-            .expect("Failed to read line");
-        page = input_line.trim().parse().expect("Input not an integer");
-        if 1 <= page && page <= max {
-            break;
-        }
-        input_line.clear();
-    }
 
     // URLを生成し、スクライピングを開始する
     let url = format!("{}/{}.{}", BASE_URL, root_url, page);
