25aa49b7d01b21bb14ba541b90c426fa926214e0
doccaico <hash_ext_luna@yahoo.co.jp>
2026-05-29 15:53:08 +0900

    Clean Up

--- rust/do/src/diary_search.rs
+++ rust/do/src/diary_search.rs
@@ -5,13 +5,13 @@ Usage:
     do.exe diary_search WORD";
 
 pub fn run(args: &[String]) -> ExitCode {
-    if args.len() == 1 || args.len() > 2 {
-        eprintln!("{}", HELP_MSG);
-        return ExitCode::FAILURE;
-    }
-    if args[1] == "-h" || args[1] == "--help" {
+    if args.len() == 2 && (args[1] == "-h" || args[1] == "--help") {
         println!("{}", HELP_MSG);
         return ExitCode::SUCCESS;
+    }
+    if args[2] != 2 {
+        eprintln!("{}", HELP_MSG);
+        return ExitCode::FAILURE;
     }
 
     const DIARY_DIR: &str = r"C:\Users\doccaico\Dropbox\diary";
