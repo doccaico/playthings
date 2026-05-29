27dfd69e5a61fa6a8116a2e464fe8298b0466822
doccaico <hash_ext_luna@yahoo.co.jp>
2026-05-29 15:47:42 +0900

    Clean Up

--- rust/do/src/shitaraba.rs
+++ rust/do/src/shitaraba.rs
@@ -1,49 +1,51 @@
 use regex::Regex;
-use std::process::{Command, ExitCode};
+use std::io::Write;
+use std::process::{Command, ExitCode, Stdio};
 
 const HELP_MSG: &str = "
 Usage:
     do.exe shitaraba GENRE ID NUMBER";
 
+fn convert_cp(code_point: &str) -> Result<String, String> {
+    let cp_u32 = u32::from_str_radix(code_point, 10)
+        .map_err(|_| "failed to 'u32::from_str_radix'".to_string())?;
+
+    char::from_u32(cp_u32)
+        .map(|c| c.to_string())
+        .ok_or_else(|| "failed to 'char::from_u32'".to_string())
+}
+
 pub fn run(args: &[String]) -> ExitCode {
-    if args.len() == 1 || args.len() != 4 {
-        eprintln!("{}", HELP_MSG);
-        return ExitCode::FAILURE;
-    }
-    if args[1] == "-h" || args[1] == "--help" {
+    if args.len() == 2 && (args[1] == "-h" || args[1] == "--help") {
         println!("{}", HELP_MSG);
         return ExitCode::SUCCESS;
+    }
+    if args.len() != 4 {
+        eprintln!("{}", HELP_MSG);
+        return ExitCode::FAILURE;
     }
 
     let (genre, id, number) = (&args[1], &args[2], &args[3]);
 
     let url = format!("https://jbbs.shitaraba.net/bbs/read.cgi/{genre}/{id}/{number}/l50");
 
-    let output = Command::new("cmd")
+    let output = match Command::new("cmd")
         .args([
             "/C",
-            &format!("curl {url} | busybox64u iconv -f EUC-JP -t UTF-8"),
+            &format!("curl -sSL {url} | busybox64u iconv -f EUC-JP -t UTF-8"),
         ])
         .output()
-        .expect("failed to 'curl and busybox64u iconv' process");
-
-    let re = match Regex::new(r"<dt(?ms)\b.+?<b>(\w+?)</b>.+?：(.+?)</dt>.+?<dd>(.+?)</dd>") {
-        Ok(re) => re,
+    {
+        Ok(out) => out,
         Err(_) => {
-            eprintln!(
-                r#"failed to 'Regex::new("<dt(?ms)\b.+?<b>(\w+?)</b>.+?：(.+?)</dt>.+?<dd>(.+?)</dd>")'"#
-            );
+            eprintln!("failed to 'curl and busybox64u iconv'");
             return ExitCode::FAILURE;
         }
     };
 
-    let re_emoji = match Regex::new(r"&#(\d+?);") {
-        Ok(re) => re,
-        Err(_) => {
-            eprintln!(r#"failed to 'Regex::new("&#(\d+?);")'"#);
-            return ExitCode::FAILURE;
-        }
-    };
+    let re = Regex::new(r"<dt(?ms)\b.+?<b>(\w+?)</b>.+?：(.+?)</dt>.+?<dd>(.+?)</dd>")
+        .expect("failed to compile main regex");
+    let re_emoji = Regex::new(r"&#(\d+?);").expect("failed to compile emoji regex");
 
     let contents = match String::from_utf8(output.stdout) {
         Ok(contens) => contens,
@@ -54,47 +56,64 @@ pub fn run(args: &[String]) -> ExitCode {
     };
 
     let mut dates = vec![];
-    let mut err_msg: &str = "";
     for (_, [name, date, post]) in re.captures_iter(&contents).map(|c| c.extract()) {
-        let (new_name, new_date, new_post) = {
-            let date = date.trim_ascii_end();
-            let post = post
-                .trim_ascii()
-                .replace("<br>          <br>", "")
-                .replace("<br>", "");
-            let post = re_emoji
-                .replace_all(&post, |caps: &regex::Captures| {
-                    let code_point = match u32::from_str_radix(&caps[1], 10) {
-                        Ok(cp) => cp,
-                        Err(_) => {
-                            err_msg = "failed to 'u32::from_str_radix'";
-                            return String::new();
-                        }
-                    };
-                    let emoji = match char::from_u32(code_point) {
-                        Some(emoji) => emoji,
-                        None => {
-                            err_msg = "failed to 'char::from_u32'";
-                            return String::new();
-                        }
-                    };
-                    emoji.to_string()
-                })
-                .into_owned();
-            (name, date, post)
-        };
-        dates.push((new_name, new_date, new_post));
+        let date = date.trim_ascii_end();
+        let post = post
+            .trim_ascii()
+            .replace("<br>          <br>", "")
+            .replace("<br>", "");
+
+        let mut error_msg = None;
+        let post = re_emoji.replace_all(&post, |caps: &regex::Captures| {
+            if error_msg.is_some() {
+                return "".to_string();
+            }
+            match convert_cp(&caps[1]) {
+                Ok(emoji) => emoji,
+                Err(e) => {
+                    error_msg = Some(e);
+                    "".to_string()
+                }
+            }
+        });
+
+        if let Some(e) = error_msg {
+            eprintln!("{}", e);
+            return ExitCode::FAILURE;
+        }
+
+        dates.push((name, date, post.into_owned()));
     }
 
-    if "" != err_msg {
-        eprintln!("{}", err_msg);
-        return ExitCode::FAILURE;
+    let mut child = match Command::new("less")
+        .args(["-R", "--silent"])
+        .stdin(Stdio::piped())
+        .spawn()
+    {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("failed to spawn 'less'");
+            return ExitCode::FAILURE;
+        }
+    };
+
+    if let Some(mut stdin) = child.stdin.take() {
+        for (name, date, post) in dates {
+            if let Err(_) = writeln!(
+                stdin,
+                "\x1b[36m{name}\x1b[0m: \x1b[32m{date}\x1b[0m\n{post}"
+            ) {
+                eprintln!("failed to write to less stdin");
+                return ExitCode::FAILURE;
+            }
+        }
     }
 
-    for v in dates {
-        println!("{}", v.2);
+    match child.wait() {
+        Ok(status) if status.success() => ExitCode::SUCCESS,
+        _ => {
+            eprintln!("less exited with an error");
+            ExitCode::FAILURE
+        }
     }
-
-    // print!("{} ", String::from_utf8_lossy(&output.stdout));
-    ExitCode::SUCCESS
 }
